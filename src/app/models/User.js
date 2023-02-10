035046082448744f7cbb811bea28479c984965d8
TranAnhTu185 <95409830+TranAnhTu185@users.noreply.github.com>
2023-02-10 15:47:04 +0700

    update code models user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 const User = new Schema({
-    email: {type: String, require: true,},
+    email: {type: String, require: true, unique: true},
     password: {type: String, require:true,},
 }, {
     timestamps: true,
